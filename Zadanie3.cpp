7697a451cb168255a926091ae85bc7478a2af8b5
asasin156 <151502369+asasin156@users.noreply.github.com>
2023-11-20 13:01:26 +0300

    Add files via upload

---
+++ Zadanie3.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int sum = 0;
+	cout << "Введите начало диапазона:" << endl;
+	int nachdiap;
+	cin >> nachdiap;
+	cout << "Введите окончание диапазона:" << endl;
+	int enddiap;
+	cin >> enddiap;
+	cout << " " << endl;
+	for (int i = nachdiap; i <= enddiap; i++) {
+		if (i % 2 == 0) {
+			
+			sum += i;
+		}
+		
+	}
+	cout <<"Сумма всех четных чисел в заданном диапозоне равна " << sum << endl;
+}
+
+
